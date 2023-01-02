30a73b3c4e9491576fc6d2e872d37cd21c01b8bf
SkyfectaGameDev <skyfecta@gmail.com>
2023-01-02 14:13:33 +0000

    Session 1 - Background Rendering & Parallax Scrolling
    Learned text rendering, simple background rendering, foreground rendering, palettes, scrolling, parallax scrolling and code formatting.

---
+++ Background Display Test/res/resources.res
@@ -0,0 +1,2 @@
+IMAGE bg1 "background2.png" BEST ALL
+IMAGE fg1 "foreground2.png" BEST ALL
--- Background Display Test/src/main.c
+++ Background Display Test/src/main.c
@@ -1,11 +1,40 @@
 #include <genesis.h>
+#include <resources.h>
+
+// ----- Variables -----
+u16 ind = TILE_USER_INDEX;																					// Setting up graphics tiles into VRAM.
+int hscroll_offeset_bg = 0;																					// Background horizontal scrolling offset value.
+int hscroll_offeset_fg = 0;																					// Background horizontal scrolling offset value.
 
 int main()
 {
-	VDP_drawText("Skyfecta Games Presents", 10, 13);
+	// ----- Background Rendering -----
+	PAL_setPalette(PAL0, bg1.palette->data, DMA);															// Assigning pallette 0 to the colours in background 1's image.
+	VDP_drawImageEx(BG_B, &bg1, TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE, ind), 0, -4, FALSE, TRUE);		// Draws background image on the background plane from the top-left of the screen.
+	ind += bg1.tileset->numTile;																			// Enables us to extract tiles from image without overwriting the background's existing ones.
 
+	// ----- Foreground Rendering -----
+	PAL_setPalette(PAL1, fg1.palette->data, DMA);															// Assigning pallette 1 to the colours in foreground 1's image.
+	VDP_drawImageEx(BG_A, &fg1, TILE_ATTR_FULL(PAL1, FALSE, FALSE, FALSE, ind), 0, -4, FALSE, TRUE);			// Draws foreground image on the foreground plane from the top-left of the screen.
+
+	// ----- Text Rendering -----
+	VDP_drawText("Skyfecta Games Presents", 8, 13);															// Draws text at designated position.
+
+	// ----- Background Scroll Setup -----
+	VDP_setScrollingMode(HSCROLL_PLANE, VSCROLL_PLANE);														// Enables scrolling by planes, and how much it scrolls by.
+
+	// ----- Repeat Every Frame -----
 	while(1)
 	{        
+		// ----- Background Scrolling -----
+		hscroll_offeset_bg -= 1;
+		VDP_setHorizontalScroll(BG_B, hscroll_offeset_bg);													// Scrolls the background plane by the assigned amount.
+
+		// ----- Foreground Scrolling -----
+		hscroll_offeset_fg -= 2;
+		VDP_setHorizontalScroll(BG_A, hscroll_offeset_fg);													// Scrolls the background plane by the assigned amount.
+
+		SYS_doVBlankProcess();																				// Refresh screen every frame.
 		VDP_waitVSync();
 	}
 	return (0);
